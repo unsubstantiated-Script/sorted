71287951f65763f42f8f31fc4e10715cc079a66c
Justin Fulton <jusfulton@gmail.com>
2020-08-28 12:47:04 -0700

    project complete

--- script.js
+++ script.js
@@ -22,15 +22,98 @@ createList();
 
 function createList() {
     [...codingLanguages]
-    .forEach((language, index) => {
-        const listItem = document.createElement('li');
-        listItem.setAttribute('data-index', index);
+    .map(a => ({
+            value: a,
+            sort: Math.random()
+        }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(a => a.value)
+        .forEach((language, index) => {
+            const listItem = document.createElement('li');
 
-        listItem.innerHTML = `
-            <span className="number">${index ++ 1}</span>
-            <div className="draggable" draggable='true'>
-            <p className="language-name">${language}</p>
+            listItem.setAttribute('data-index', index);
+
+            listItem.innerHTML = `
+            <span class="number">${index + 1}</span>
+            <div class="draggable" draggable="true">
+                <p class="language-name">${language}</p>
+                <i class="fa fa-grip-horizontal"></i>
             </div>
         `;
+
+            listItems.push(listItem);
+
+            draggableList.appendChild(listItem);
+        });
+
+    addEventListeners();
+};
+
+function dragStart() {
+    // console.log('Event: ', 'dragstart');
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+function dragEnter() {
+    // console.log('Event: ', 'dragenter');
+    this.classList.add('over');
+}
+
+function dragLeave() {
+    // console.log('Event: ', 'dragleave');
+    this.classList.remove('over');
+}
+
+function dragOver(e) {
+    // console.log('Event: ', 'dragover');
+    e.preventDefault();
+}
+
+function dragDrop() {
+    // console.log('Event: ', 'drop');
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over');
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+}
+
+// Check the order of list items
+function checkOrder() {
+    listItems.forEach((listItem, index) => {
+        const personName = listItem.querySelector('.draggable').innerText.trim();
+
+        if (personName !== codingLanguages[index]) {
+            listItem.classList.add('incorrect')
+        } else {
+            listItem.classList.remove('incorrect')
+            listItem.classList.add('correct')
+        }
     })
 }
+
+function addEventListeners() {
+    const draggables = document.querySelectorAll('.draggable');
+    const dragListItems = document.querySelectorAll('.draggable-list li');
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart);
+    });
+
+    dragListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver);
+        item.addEventListener('drop', dragDrop);
+        item.addEventListener('dragenter', dragEnter);
+        item.addEventListener('dragleave', dragLeave);
+    });
+}
+
+check.addEventListener('click', checkOrder)
